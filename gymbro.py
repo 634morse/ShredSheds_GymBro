0ac8270f2ddeaf89ffe3006a01873be030d38a77
unknown <634morse@gmail.com>
2023-09-17 23:54:45 -0400

    DEVELOPING

--- gymbro.py
+++ gymbro.py
@@ -4,10 +4,11 @@ from modules import db_handler
 
 if __name__ == "__main__":
     #Initializing Database
-    db_name = db_handler.Database_Intitialization('gymbro.db')
-    db_name.create_gymbro_db()
+    db_handler.Database_Intitialization('gymbro.db')
 
     #Initializing db_handler for use in the Gymbro class
     db_handling = db_handler.Database_Handler('gymbro.db')
+
+    #Running the application
     app = gui.GymBro()
     app.mainloop()
--- modules/db_handler.py
+++ modules/db_handler.py
@@ -5,29 +5,36 @@ import datetime
 class Database_Intitialization:
     def __init__(self, database_name):
         self.database_name = database_name
-        self.create_table_weightmaxes()
+        self.connection = sqlite3.connect(database_name)
+        self.create_tables()
 
-    def create_gymbro_db(self):
-        database = sqlite3.connect(self.database_name)
-        cursor = database.cursor()
-        database.commit()
-        database.close()
+    def __del__(self):
+        self.connection.close()
+
+    def create_tables(self):
+        self.create_table_weightmaxes()
+        self.create_table_exercises()
 
     def create_table_weightmaxes(self):
-        connection = sqlite3.connect(self.database_name)
-        cursor = connection.cursor()
-        table = """ CREATE TABLE IF NOT EXISTS WEIGHT_MAXES (
-                Exercise VARCHAR(100),
-                Weight INT,
-                Reps INT,
-                Date DATETIME
-                );
-                """
-        cursor.execute(table)
-        connection.commit()
-        connection.close()
+        with self.connection:
+            cursor = self.connection.cursor()
+            table = """ CREATE TABLE IF NOT EXISTS WEIGHT_MAXES (
+                    Exercise VARCHAR(100),
+                    Weight INT,
+                    Reps INT,
+                    Date DATETIME
+                    );
+                    """
+            cursor.execute(table)
 
-
+    def create_table_exercises(self):
+        with self.connection:
+            cursor = self.connection.cursor()
+            table = """ CREATE TABLE IF NOT EXISTS EXERCISES (
+                    Exercise VARCHAR(100)
+                    );
+                    """
+            cursor.execute(table)
 
 class Database_Handler:
     def __init__(self, db_file):
@@ -62,46 +69,46 @@ class Database_Handler:
 
 class Get_Est_Rep_Weights:
     def __init__(self, exercise, rep_range):
-        self.exercise_max = self.get_maxes(exercise)
-        self.exercise_max = self.exercise_max
+        self.exercise_max_data = self.get_maxes(exercise)
+        self.exercise_max_data = self.exercise_max_data
+        self.exercise_max = self.exercise_max_data[0]
+        self.exercise_reps = self.exercise_max_data[1]
 
-        self.est_weights = self.Epley_Est_Calc(self.exercise_max, rep_range)
+        self.est_weights = self.Epley_Est_Calc(self.exercise_max, self.exercise_reps, rep_range)
 
 
     def get_maxes (self, exercise):
         connection = sqlite3.connect('gymbro.db')
         cursor = connection.cursor()
         query = """
-                SELECT Weight
+                SELECT Weight, reps, date
                 FROM WEIGHT_MAXES
-                Where Reps = 1
-                AND Exercise = ?
+                WHERE Exercise = ?
+                order by Date DESC, reps ASC
                 LIMIT 1
                 """
         cursor.execute(query, (exercise,))
         result = cursor.fetchone()
         connection.close()
         if result is not None:
-            return result[0]
+            return result
         else:
             return None
     
-    def Epley_Est_Calc(self, weightmax, rep_range):
+    def Epley_Est_Calc(self, weightmax, reps, rep_range):
         start_str, end_str = rep_range.split('-')
         start = int(start_str)
         end = int(end_str)
         rep_range = (start_str, end_str)
         est_weights = {}
 
+        # Getting est 1rm if max weight is for more than 1 rep  
+        if reps != 1:
+            weightmax = weightmax * (1 + reps / 30)
+
+        #Calculating weight estimates
         for rep in range(start, end + 1):
             est_weight = weightmax / (1 + 0.0333 * rep)
             est_weights[rep] = round(est_weight)
         
         return est_weights
-# # est_weights = calculations.Epley_Est_Calc(result[0], "1-5")
-
-test = Get_Est_Rep_Weights("Bench Press", "1-3")
-print(test.est_weights)
-
-    
-
